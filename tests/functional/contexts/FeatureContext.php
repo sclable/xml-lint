2827e371c6b3f6012b952d3ee184fd3f9f47e080
Michael Rutz (Sclable Business Solutions GmbH) <michael.rutz@sclable.com>
2015-10-19 12:13:54 +0200

    add tests

---
+++ tests/functional/contexts/FeatureContext.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace sclable\xmlLint\tests\functional\contexts;
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use sclable\xmlLint\console\application\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context, SnippetAcceptingContext
+{
+    /** @var Application */
+    protected $application;
+
+    /** @var CommandTester */
+    protected $commandTester;
+
+    /** @var int the exit code */
+    protected $exitCode;
+
+    /** @var string */
+    protected $file;
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+        $this->application = new Application();
+
+        $command = $this->application->find('lint');
+
+        $this->commandTester = new CommandTester($command);
+
+    }
+
+    /**
+     * @Given the file :file
+     * @param $file
+     */
+    public function theFile($file)
+    {
+        $this->file = dirname(__DIR__) . '/_testdata/' . $file;
+    }
+
+    /**
+     * @When I run lint
+     */
+    public function iRunLint()
+    {
+        $this->exitCode = $this->commandTester->execute([
+            'file' => $this->file
+        ]);
+    }
+
+    /**
+     * @Then I have a return code :code
+     * @param $code
+     * @throws \Exception
+     */
+    public function iHaveAReturnCode($code)
+    {
+        $code = (int) $code;
+
+        if ($this->exitCode === null) {
+            echo $this->commandTester->getDisplay();
+            throw new \Exception('the return code was NULL.');
+        }
+
+        if ($this->exitCode !== $code) {
+            echo $this->commandTester->getDisplay();
+            throw new \Exception(sprintf(
+                "the return code does not match. \n Expected: %s\n Actual: %s",
+                $code,
+                $this->exitCode
+            ));
+        }
+    }
+}
---
+++ tests/functional/xml-lint.feature
@@ -0,0 +1,12 @@
+Feature: Check an xml file whether its structure seems to be ok or not.
+
+  Scenario: Check and confirm a file to be ok
+    Given the file "fourtytwo.xml"
+    When I run lint
+    Then I have a return code "0"
+
+  Scenario: Check a file with a missing close tag
+    Given the file "broken.xml"
+    When I run lint
+    Then I have a return code "1"
+
